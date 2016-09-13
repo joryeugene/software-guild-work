0b7d0e7eb6b85871e11a0e1ceb70ea417084ea01
Jory Pestorious <jorypestorious@outlook.com>
2016-09-13 16:45:02 -0500

    JOPEINDWRK-5 Programming by Doing -> Variables -> Using Variables

---
+++ ProgrammingByDoing/PBD-Variables/src/main/java/com/jorypestorious/pbd/variables/UsingVariables.java
@@ -0,0 +1,13 @@
+package com.jorypestorious.pbd.variables;
+
+public class UsingVariables {
+    public static void main(String[] args) {
+        int first = 113;
+        double second = 2.71828;
+        String third = "Computer Science";
+        
+        System.out.println("This is room # " + first);
+        System.out.println("e is close to " + second);
+        System.out.println("I am learning a bit about " + third);
+    }
+}
