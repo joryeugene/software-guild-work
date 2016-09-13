53857b05861212c38ac4c17e03d01f25c7264253
Jory Pestorious <jorypestorious@outlook.com>
2016-09-13 16:54:39 -0500

    JOPEINDWRK-5 Programming by Doing -> Variables -> Your Schedule

---
+++ ProgrammingByDoing/PBD-Variables/src/main/java/com/jorypestorious/pbd/variables/YourSchedule.java
@@ -0,0 +1,17 @@
+package com.jorypestorious.pbd.variables;
+
+public class YourSchedule {
+    public static void main(String[] args) {
+        System.out.print(
+                "+------------------------------------------------------------+\n" +
+                "| 1 |                          English III |       Ms. Lapan |\n" +
+                "| 2 |                          Precalculus |     Mrs. Gideon |\n" +
+                "| 3 |                         Music Theory |       Mr. Davis |\n" +
+                "| 4 |                        Biotechnology |      Ms. Palmer |\n" +
+                "| 5 |           Principles of Technology I |      Ms. Garcia |\n" +
+                "| 6 |                             Latin II |    Mrs. Barnett |\n" +
+                "| 7 |                        AP US History | Ms. Johannessen |\n" +
+                "| 8 | Business Computer Infomation Systems |       Mr. James |\n" +
+                "+------------------------------------------------------------+\n");
+    }
+}
