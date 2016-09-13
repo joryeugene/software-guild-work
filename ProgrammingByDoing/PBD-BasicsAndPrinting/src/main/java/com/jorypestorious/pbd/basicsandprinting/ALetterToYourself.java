c91443cc37c59adfb0dbfb6e8cd65c0a47c8b733
Jory Pestorious <jorypestorious@outlook.com>
2016-09-13 13:25:51 -0500

    JOPEINDWRK-5 Starting Programming by Doing assignments and finished 'A Letter to Yourself' and 'Your Initials'

---
+++ ProgrammingByDoing/PBD-BasicsAndPrinting/src/main/java/com/jorypestorious/pbd/basicsandprinting/ALetterToYourself.java
@@ -0,0 +1,17 @@
+package com.jorypestorious.pbd.basicsandprinting;
+
+public class ALetterToYourself {
+    public static void main(String[] args) {
+        System.out.println("+--------------------------------------------------+");
+        System.out.println("|                                             #### |");
+        System.out.println("|                                             #### |");
+        System.out.println("|                                             #### |");
+        System.out.println("|                                                  |");
+        System.out.println("|               Jory Eugene Pestorious             |");
+        System.out.println("|               101 Street Address                 |");
+        System.out.println("|               St. Town, MN 55555                 |");
+        System.out.println("|                                                  |");
+        System.out.println("|                                                  |");
+        System.out.println("+--------------------------------------------------+");
+    }
+}
---
+++ ProgrammingByDoing/PBD-BasicsAndPrinting/src/main/java/com/jorypestorious/pbd/basicsandprinting/YourInitials.java
@@ -0,0 +1,14 @@
+package com.jorypestorious.pbd.basicsandprinting;
+
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println("For the name Jory Eugene Pestorious...\n");
+        System.out.println("JJJJJ  EEEEE  PPPP");
+        System.out.println("  J    E      P   P");
+        System.out.println("  J    E      P   P");
+        System.out.println("  J    EEE    PPPP");
+        System.out.println("J J    E      P");
+        System.out.println("J J    E      P");
+        System.out.println(" J     EEEEE  P");
+    }
+}
