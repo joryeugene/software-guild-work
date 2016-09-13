2434eb17c7756f49ec570aef96362b511ebd2959
Jory Pestorious <jorypestorious@outlook.com>
2016-09-13 16:50:09 -0500

    JOPEINDWRK-5 Programming by Doing -> Variables -> Still Using Variables

---
+++ ProgrammingByDoing/PBD-Variables/src/main/java/com/jorypestorious/pbd/variables/StillUsingVariables.java
@@ -0,0 +1,10 @@
+package com.jorypestorious.pbd.variables;
+
+public class StillUsingVariables {
+    public static void main(String[] args) {
+        String myName = "Jory Eugene Pestorious";
+        short graduationYear = 2006;
+        
+        System.out.println("My name is " + myName + " and I graduated from high school in " + graduationYear);
+    }
+}
