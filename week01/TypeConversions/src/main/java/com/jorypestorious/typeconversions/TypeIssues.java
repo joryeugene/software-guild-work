c006767527a4e0d0492a0aeab57bfe3ef4109d63
Jory Pestorious <jorypestorious@outlook.com>
2016-09-14 12:04:27 -0500

    Week 1 - TypeIssues

--- week01/TypeConversions/src/main/java/com/jorypestorious/typeconversions/TypeIssues.java
+++ week01/TypeConversions/src/main/java/com/jorypestorious/typeconversions/TypeIssues.java
@@ -6,5 +6,10 @@ public class TypeIssues {
         System.out.println(2000000000L + 500000000);
         System.out.println(2.5 / 0);
         System.out.println(0.0 / 0.0);
+        System.out.println("3.14 + 4 == 7.14 is " + (3.14 + 4 == 7.14)); // false
+        // equality tests for doubles/floats are dangerous due to rounding/precisions issues
+        double x = 3.14, y = 4, z = x + y;
+        System.out.println(z);
+        // 7.140000000000001
     }
 }
