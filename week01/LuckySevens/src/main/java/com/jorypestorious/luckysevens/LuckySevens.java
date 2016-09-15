8dabb0d6332a70951dc56028bf7c523f67a2cc7d
Jory Pestorious <jorypestorious@outlook.com>
2016-09-15 08:38:55 -0500

    JOPEINDWRK-14 Lab 01 Lucky Sevens

---
+++ week01/LuckySevens/src/main/java/com/jorypestorious/luckysevens/LuckySevens.java
@@ -0,0 +1,44 @@
+package com.jorypestorious.luckysevens;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class LuckySevens {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        Random r = new Random();
+        
+        System.out.print("How many dollars do you have? ");
+        int bet = scan.nextInt(),
+            initialBet = bet,
+            max = bet,
+            rollsAtMax = 0,
+            totalRolls = 0;
+        
+        while (bet > 0) {
+            totalRolls++;
+            if (1 + r.nextInt(6) == 1 + r.nextInt(6)) {
+                bet +=  4;
+                if (max < bet) {
+                    max = bet;
+                    rollsAtMax = totalRolls;
+                }
+                
+            } else {
+                bet--;
+            }
+        }
+        
+        String rollsAtMaxMsg;
+        if (initialBet == max) {
+            rollsAtMaxMsg = "at the beginning";
+        } else {
+            rollsAtMaxMsg = "after " + rollsAtMax + " rolls";
+        }
+        
+        System.out.println("\nYou are broke after " + totalRolls + " rolls.\n");
+        System.out.println("You should have quit " + rollsAtMaxMsg + " when you had $" + max + ".");
+    }
+}
