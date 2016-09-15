fa0eef8cec9375f4ce44a766102d1acbd5f17c2c
Jory Pestorious <jorypestorious@outlook.com>
2016-09-15 10:55:35 -0500

    JOPEINDWRK-15 Lab 02: Interest Calculator Add choice for compound interest period

--- week01/InterestCalculator/src/main/java/com/jorypestorious/interestcalculator/InterestCalculator.java
+++ week01/InterestCalculator/src/main/java/com/jorypestorious/interestcalculator/InterestCalculator.java
@@ -8,17 +8,30 @@ public class InterestCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
-        System.out.println("QUARTERLY INTEREST CALCULATOR");
+        System.out.println("INTEREST CALCULATOR");
         System.out.println("Enter numbers without any formatting (no % or ,)\n");
         System.out.print("Annual interest rate: ");
         double interestRate = scan.nextDouble();
-//        quarterly, monthly, or daily
-//interest compound periods. 
-        
+        System.out.print("Compound period (quarterly, monthly, or daily): ");
+        String compoundPeriod = scan.next();
         System.out.print("Initial principal: ");
         double principal = scan.nextDouble();
         System.out.print("Number of years money will stay in fund: ");
         int years = scan.nextInt();
+        
+        int compoundPeriodCode;
+        
+        switch (compoundPeriod) {
+            case "quarterly": compoundPeriodCode = 4;
+                              break;
+            case "monthly": compoundPeriodCode = 12;
+                            break;
+            case "daily": compoundPeriodCode = 365;
+                          break;
+            default: compoundPeriodCode = 1;
+                     System.out.println("\nDefaulting to annual compound period.");
+                     break;
+        }
         
         Calendar now = Calendar.getInstance();   // Gets the current date and time
         int currentYear = now.get(Calendar.YEAR);
@@ -34,8 +47,8 @@ public class InterestCalculator {
             System.out.print("$" + df.format(principal) + "\t");
             if ( principal < 1000) System.out.print("\t");
             double tempPrincipal = principal;
-            for (int i = 0; i < 4 ; i++) {
-                principal *= (1 + ((interestRate/4) / 100));
+            for (int i = 0; i < compoundPeriodCode ; i++) {
+                principal *= (1 + ((interestRate/compoundPeriodCode) / 100));
             }
             double yearInterest = principal - tempPrincipal;
             System.out.print("$" + df.format(yearInterest) + "\t");
