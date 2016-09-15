37d3b61e6f7e6eeffb3072a677bd5c635f921f87
Jory Pestorious <jorypestorious@outlook.com>
2016-09-15 10:36:34 -0500

    WindowMasterV2 refactor with method

--- WindowMasterV2/src/main/java/com/jorypestorious/windowmasterv2/WindowMasterV2.java
+++ WindowMasterV2/src/main/java/com/jorypestorious/windowmasterv2/WindowMasterV2.java
@@ -4,17 +4,19 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class WindowMasterV2 {
-    public static void main(String[] args) {
+    
+    public static double getInput(String prompt) {
+        System.out.print(prompt + " ");
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter window height (in feet):");
-        Double height = scan.nextDouble();
-        System.out.println("Enter window width (in feet):");
-        Double width = scan.nextDouble();
-        System.out.println("Cost of the glass per square foot:");
-        Double glassCost = scan.nextDouble();
-        System.out.println("Cost of trim per linear foot:");
-        Double trimCost = scan.nextDouble();
-        
+        return scan.nextDouble();
+    }
+    
+    public static void main(String[] args) {
+        double height = getInput("Enter window height (in feet):");
+        double width = getInput("Enter window width (in feet):");
+        double glassCost = getInput("Cost of the glass per square foot:");
+        double trimCost = getInput("Cost of trim per linear foot:");
+
         Double areaOfWindow = height * width;
         Double perimeterOfWindow = 2 * (height + width);
         
@@ -24,10 +26,10 @@ public class WindowMasterV2 {
         
         DecimalFormat df = new DecimalFormat("0.00");
         
-        System.out.println("Window height is " + height + " ft and width is " + width + " ft");
+        System.out.println("\nWindow height is " + height + " ft and width is " + width + " ft");
         System.out.println("Window area is " + areaOfWindow + " ft² and perimeter is " + perimeterOfWindow + " ft");
         System.out.println("The glass cost is $" + df.format(glassCost) + " per square foot, making the total glass cost $" + df.format(costOfGlass));
         System.out.println("The trim cost is $" + df.format(trimCost) + " per linear foot, making the total trim cost $" + df.format(costOfTrim));
-        System.out.println("The total overall cost is $" + df.format(totalCost));       
+        System.out.println("The total overall cost is $" + df.format(totalCost));
     }
 }
