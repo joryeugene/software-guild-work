fbe1d3b215f5f3ac6de460a3d26b0298c32a7a04
Jory Pestorious <jorypestorious@outlook.com>
2016-09-16 16:22:52 -0500

    Make Interest Calculator lab look nice for code review

--- week01/InterestCalculator/src/main/java/com/jorypestorious/interestcalculator/InterestCalculator.java
+++ week01/InterestCalculator/src/main/java/com/jorypestorious/interestcalculator/InterestCalculator.java
@@ -5,33 +5,35 @@ import java.util.Calendar;
 import java.util.Scanner;
 
 public class InterestCalculator {
+    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
-        System.out.println("INTEREST CALCULATOR");
-        System.out.println("Enter numbers without any formatting (no % or ,)\n");
+        System.out.println("=========================\n" +
+                           "   INTEREST CALCULATOR\n" +
+                           "=========================\n");
+        
         System.out.print("Annual interest rate: ");
-        double interestRate = scan.nextDouble();
-        System.out.print("Compound period (quarterly, monthly, or daily): ");
-        String compoundPeriod = scan.next();
+        double interestRate = scanCurrencyAndPercentage();
+        
+        boolean invalidInput;
+        int compoundPeriodCode;
+                
+        do {
+            System.out.print("Compound period (quarterly, monthly, or daily): ");
+            String compoundPeriod = scan.nextLine();
+            compoundPeriodCode = getCompoundPeriodCode(compoundPeriod);
+            
+            invalidInput = false;
+            if (compoundPeriodCode == 0) invalidInput = true;
+            
+        } while (invalidInput);
+        
         System.out.print("Initial principal: ");
-        double principal = scan.nextDouble();
+        double principal = scanCurrencyAndPercentage();
+        
         System.out.print("Number of years money will stay in fund: ");
         int years = scan.nextInt();
-        
-        int compoundPeriodCode;
-        
-        switch (compoundPeriod) {
-            case "quarterly": compoundPeriodCode = 4;
-                              break;
-            case "monthly": compoundPeriodCode = 12;
-                            break;
-            case "daily": compoundPeriodCode = 365;
-                          break;
-            default: compoundPeriodCode = 1;
-                     System.out.println("\nDefaulting to annual compound period.");
-                     break;
-        }
         
         Calendar now = Calendar.getInstance();   // Gets the current date and time
         int currentYear = now.get(Calendar.YEAR);
@@ -39,21 +41,50 @@ public class InterestCalculator {
         
         DecimalFormat df = new DecimalFormat("###,##0.00");
         
-        System.out.println("\nYear\tStarting\tInterest\tYear-end");
-        System.out.println("-------------------------------------------------------------");
+        System.out.println("\nYear\tStarting\tInterest\tYear-end\n" +
+                           "-------------------------------------------------------");
+        
         while (currentYear != targetYear) {
             System.out.print(currentYear + "\t");
             currentYear++;
+            
             System.out.print("$" + df.format(principal) + "\t");
             if ( principal < 1000) System.out.print("\t");
-            double tempPrincipal = principal;
+            
+            double oldPrincipal = principal;
+            
             for (int i = 0; i < compoundPeriodCode ; i++) {
-                principal *= (1 + ((interestRate/compoundPeriodCode) / 100));
+                principal *= (1 + ((interestRate / compoundPeriodCode) / 100));
             }
-            double yearInterest = principal - tempPrincipal;
+            
+            double yearInterest = principal - oldPrincipal;
             System.out.print("$" + df.format(yearInterest) + "\t");
             if (yearInterest < 1000) System.out.print("\t");
-            System.out.println("$" + df.format(principal) + "\t");
+            
+            System.out.println("$" + df.format(principal));
         }
+        
+        System.out.println("-------------------------------------------------------");
     }
+    
+    static double scanCurrencyAndPercentage() {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        return Double.parseDouble(input.replaceAll("[$,%]", ""));
+    }
+    
+    static int getCompoundPeriodCode(String compoundPeriod) {
+        switch (compoundPeriod) {
+                case "quarterly":
+                    return 4;
+                case "monthly":
+                    return 12;
+                case "daily":
+                    return 365;
+                default:
+                    System.out.println("INVALID INPUT");
+                    return 0;
+            }
+    }
+    
 }
