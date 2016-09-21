cae0d864792dfa8ae678c41aaed0f054f69a72df
Jory Pestorious <jorypestorious@outlook.com>
2016-09-21 10:37:47 -0500

    Refactor Simple Calculator ConsoleIO

--- week02/SimpleCalculator/src/main/java/com/jorypestorious/simplecalculator/ConsoleIO.java
+++ week02/SimpleCalculator/src/main/java/com/jorypestorious/simplecalculator/ConsoleIO.java
@@ -8,62 +8,102 @@ public class ConsoleIO {
     
     public int promptInt(String prompt) {
         System.out.print(prompt);
-        return scan.nextInt();
+        
+        while (!scan.hasNextInt()) {
+            System.out.print("Enter a number: ");
+            scan.next();
+        }
+        
+        int input = scan.nextInt();
+        clearBuffer();
+        return input;
     }
     
     public int promptInt(String prompt, int min, int max) {
         int input;
-        System.out.print(prompt);
-        input = scan.nextInt();
         
-        while (input > max || input < min) {
-            System.out.print("Number must be between " + min + " and " + max);
-        }
+        do {
+            System.out.print(prompt);
+            while (!scan.hasNextInt()) {
+                System.out.print("Enter a number between " + min + " and " + max);
+                scan.next();
+            }
+            input = scan.nextInt();
+        } while (input > max || input < min);
         
+        clearBuffer();
         return input;
     }
     
     public String promptString(String prompt) {
         System.out.print(prompt);
-        return scan.next();
+        return scan.nextLine();
     }
     
     public float promptFloat(String prompt) {
         System.out.print(prompt);
-        return scan.nextFloat();
+        
+        while (!scan.hasNextFloat()) {
+            System.out.print("Enter a number: ");
+            scan.next();
+        }
+        
+        float input = scan.nextFloat();
+        clearBuffer();
+        return input;
     }
     
     public float promptFloat(String prompt, float min, float max) {
         float input;
-        System.out.print(prompt);
-        input = scan.nextFloat();
         
-        while (input > max || input < min) {
-            System.out.print("Number must be between " + min + " and " + max);
-        }
+        do {
+            System.out.print(prompt);
+            while (!scan.hasNextFloat()) {
+                System.out.print("Enter a number between " + min + " and " + max);
+                scan.next();
+            }
+            input = scan.nextFloat();
+        } while (input > max || input < min);
         
+        clearBuffer();
         return input;
     }
     
     public double promptDouble(String prompt) {
         System.out.print(prompt);
-        return scan.nextDouble();
+        
+        while (!scan.hasNextDouble()) {
+            System.out.print("Enter a number: ");
+            scan.next();
+        }
+        
+        double input = scan.nextDouble();
+        clearBuffer();
+        return input;
     }
     
     public double promptDouble(String prompt, double min, double max) {
         double input;
-        System.out.print(prompt);
-        input = scan.nextDouble();
         
-        while (input > max || input < min) {
-            System.out.print("Number must be between " + min + " and " + max);
-        }
+        do {
+            System.out.print(prompt);
+            while (!scan.hasNextDouble()) {
+                System.out.print("Enter a number between " + min + " and " + max);
+                scan.next();
+            }
+            input = scan.nextDouble();
+        } while (input > max || input < min);
         
+        clearBuffer();
         return input;
     }
     
-    public void printString(String prompt) {
-        System.out.println(prompt);
+    public void printString(String out) {
+        System.out.println(out);
     }
-
+    
+    public void clearBuffer() {
+        scan.nextLine();
+    }
+    
 }
