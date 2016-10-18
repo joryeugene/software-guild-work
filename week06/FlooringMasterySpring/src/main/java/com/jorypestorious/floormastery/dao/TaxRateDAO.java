eba7b0e52debae7ffc8cc197f47a8c2987ffb032
Jory Pestorious <jorypestorious@outlook.com>
2016-10-18 14:58:47 -0500

    FloorMasterySpring Lab - Complete with Spring dependency injection, etc

---
+++ week06/FlooringMasterySpring/src/main/java/com/jorypestorious/floormastery/dao/TaxRateDAO.java
@@ -0,0 +1,10 @@
+package com.jorypestorious.floormastery.dao;
+
+import com.jorypestorious.floormastery.dto.TaxRate;
+import java.util.List;
+
+public interface TaxRateDAO {
+    
+    List<TaxRate> run();
+
+}
