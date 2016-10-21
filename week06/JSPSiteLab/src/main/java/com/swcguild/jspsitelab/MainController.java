c679f3b9588fecb52eb87e56c66ea2fe397c7ea4
Jory Pestorious <jorypestorious@outlook.com>
2016-10-21 13:57:18 -0500

    Move unit converter logic into separate controller

--- week06/JSPSiteLab/src/main/java/com/swcguild/jspsitelab/MainController.java
+++ week06/JSPSiteLab/src/main/java/com/swcguild/jspsitelab/MainController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping({"/apps"})
-public class WebController {
+public class MainController {
     
     @RequestMapping(value="luckysevens", method=RequestMethod.GET)
     public String displayLuckySevens(HttpServletRequest req, Model model) {
@@ -174,74 +174,5 @@ public class WebController {
         return "TipCalculator02";
     }
     
-    @RequestMapping(value="unitconverter", method=RequestMethod.GET)
-    public String displayUnitConverter(HttpServletRequest req, Model model) {
-        return "UnitConverter01";
-    }
-    
-    @RequestMapping(value="unitconverterresults", method=RequestMethod.POST)
-    public String displayUnitConverterResults(HttpServletRequest req, Model model) { 
-        
-        String converter = req.getParameter("converter");
-        String startingUnit = req.getParameter("startingUnit");
-        String endingUnit = req.getParameter("endingUnit");
-        double value = Integer.parseInt(req.getParameter("value"));
-        double result = -1.0;
-        
-        switch (converter) {
-            case "length":
-                result = convertLength(startingUnit, endingUnit, value);
-                break;
-            case "temperature":
-                result = convertTemperature(startingUnit, endingUnit, value);
-                break;
-        }
-        
-        model.addAttribute("result", result);
-        
-        return "UnitConverter02";
-    }
-    
-    private double convertLength(String startingUnit, String endingUnit, double value) {
-        double base = 0.0;
-        double result = 0.0;
-        
-        switch (startingUnit) {
-            case "foot": base = value;
-                break;
-            case "yard": base = value * 3;
-                break;
-        }
-        
-        switch(endingUnit) {
-            case "foot": result = base;
-                break;
-            case "yard": result = base / 3;
-                break;
-        }
-
-        return result;        
-    }
-    
-    private double convertTemperature(String startingUnit, String endingUnit, double value) {
-        double base = 0.0;
-        double result = 0.0;
-        
-        switch (startingUnit) {
-            case "celsius": base = value;
-                break;
-            case "farenheit": base = (value - 32) / 1.8;
-                break;
-        }
-        
-        switch(endingUnit) {
-            case "celsius": result = base;
-                break;
-            case "farenheit": result = base * 1.8 + 32;
-                break;
-        }
-
-        return result;        
-    }
-    
+      
 }
