48240b258a511bffe5d98d1e48bcaf6de3093c5b
Jory Pestorious <jorypestorious@outlook.com>
2016-10-28 11:43:03 -0500

    DVD Library Spring MVC - Put dvds in a scrollable table and add Add DVD form to that main page

--- week07/DVDLibraryMVC/src/main/java/com/jorypestorious/dvdlibrarymvc/controller/HomeControllerNoAjax.java
+++ week07/DVDLibraryMVC/src/main/java/com/jorypestorious/dvdlibrarymvc/controller/HomeControllerNoAjax.java
@@ -16,18 +16,34 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeControllerNoAjax {
 
-    // Reference to our DAO
     private final DVDLibraryDao dao;
+    private boolean firstStart = true;
 
     @Inject
     public HomeControllerNoAjax(DVDLibraryDao dao) {
         this.dao = dao;
-        dao.addDVD(new DVD("Finding Nemo", "2003", "G", "Andrew Stanton", "Pixar", "Stupid fish swims around aimlessly"));
-        dao.addDVD(new DVD("Spirited Away", "2001", "PG", "Hayao Miyazaki", "Studio Ghibli", "Young girl does some dirty work at a bathhouse"));
     }
-
+    
+    private void populateLibrary() {
+        dao.addDVD(new DVD("Finding Nemo", "2003", "G", "Andrew Stanton", "Pixar", "Stupid fish swims around aimlessly"));
+        dao.addDVD(new DVD("Spirited Away", "2001", "PG", "Hayao Miyazaki", "Studio Ghibli", "Girl does some dirty work at a bathhouse"));
+        dao.addDVD(new DVD("Mr. Nobody", "2009", "R", "Jaco Van Dormael", "Pan Européenne", "Future guy with face tattoos interviews old crazy man"));
+        dao.addDVD(new DVD("Inside Out", "2015", "PG", "Pete Docter", "Pixar", "Lunatics inside our heads and drive all our actions"));
+        dao.addDVD(new DVD("Howl's Moving Castle", "2004", "PG", "Hayao Miyazaki", "Studio Ghibli", "Crazy guy with cool mobile home"));
+        dao.addDVD(new DVD("My Neighbor Totoro", "1988", "G", "Hayao Miyazaki", "Studio Ghibli", "Scary monsters try to eat two kids"));
+        dao.addDVD(new DVD("Spring, Summer, Fall, Winter... and Spring", "2003", "R", "Ki-duk Kim", "Korea Pictures", "Buddhist gets horny and goes to town"));
+        dao.addDVD(new DVD("Princess Mononoke", "1997", "PG-13", "Hayao Miyazaki", "Studio Ghibli", "Cute little forest spirits get mad"));
+        dao.addDVD(new DVD("Amélie", "2001", "R", "Jean-Pierre Jeunet", "Claudie Ossard Productions", "Weird cute French girl does things around town"));
+        dao.addDVD(new DVD("Kiki's Delivery Service", "1989", "G", "Hayao Miyazaki", "Studio Ghibli", "Young witch leads on nerd boy while starting a business"));
+    }
+    
     @RequestMapping(value = "/displayDVDLibraryNoAjax", method = RequestMethod.GET)
     public String displayDVDLibraryNoAjax(Model model) {
+        if (firstStart) {
+            populateLibrary();
+            firstStart = false;
+        }
+        
         List<DVD> dvdList = dao.getAllDVDs();
         model.addAttribute("dvdList", dvdList);
         return "displayDVDLibraryNoAjax";
