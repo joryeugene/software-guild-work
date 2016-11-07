a735b22047aea5df85fc236a1aee00b14abe0538
Jory Pestorious <jorypestorious@outlook.com>
2016-11-07 11:06:48 -0600

    DVD Library - Remove SafeHtml annotation

--- week08/DVDLibraryMVC-V2/src/main/java/com/jorypestorious/dvdlibrarymvc/dto/DVD.java
+++ week08/DVDLibraryMVC-V2/src/main/java/com/jorypestorious/dvdlibrarymvc/dto/DVD.java
@@ -3,7 +3,6 @@ package com.jorypestorious.dvdlibrarymvc.dto;
 import javax.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
-import org.hibernate.validator.constraints.SafeHtml;
 
 public class DVD {
     
